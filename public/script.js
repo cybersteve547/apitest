3b1ca96e9e9c67580d2836f45aec9f3c831052b8
cybersteve547 <minecraftstevso@gmail.com>
2025-06-21 19:28:31 +0000

    script

---
+++ public/script.js
@@ -0,0 +1,35 @@
+console.log = (...args) => {
+  const output = args.join(" ");
+  const consoleDiv = document.getElementById("console");
+  consoleDiv.innerHTML += output + "<br>";
+  consoleDiv.scrollTop = consoleDiv.scrollHeight;
+};
+
+const workspace = Blockly.inject('blocklyDiv', {
+  toolbox: document.getElementById('toolbox'),
+});
+
+async function runCode() {
+  console.log("test");
+  try {
+    const code = Blockly.JavaScript.workspaceToCode(workspace);
+  } catch (e) {
+    console.log(e);
+  }
+  console.log('Generated code:', code);
+
+  const response = await fetch('/api/run', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ code }),
+  });
+
+  const result = await response.json();
+  console.log(result);
+  try {
+    eval(result);
+  } catch (e) {
+    console.log(e);
+  }
+  alert('Backend says: ' + result.output);
+}
