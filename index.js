f62745a124d3f03a7c03d06f40bed55b4a1f7ba3
cybersteve547 <minecraftstevso@gmail.com>
2025-06-21 16:32:29 +0000

    .

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Render + Node.js!');
+  res.send("cool api thing here");
+  console.log(`ooo somebody looked at it 😄😄😄 ${req}`)
 });
 
 app.listen(port, () => {
