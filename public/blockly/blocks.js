7d2e180075666c392afa1ae8d64f498dbfbd9482
cybersteve547 <minecraftstevso@gmail.com>
2025-06-21 19:39:11 +0000

    move

--- public/blockly/blocks.js
+++
@@ -1,8 +0,0 @@
-Blockly.Blocks['say_hello'] = {
-  init: function() {
-    this.appendDummyInput().appendField("say hello");
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(160);
-  }
-};
--- public/script.js
+++ public/script.js
@@ -5,6 +5,15 @@ console.log = (...args) => {
   consoleDiv.scrollTop = consoleDiv.scrollHeight;
 };
 
+Blockly.Blocks['say_hello'] = {
+  init: function() {
+    this.appendDummyInput().appendField("say hello");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(160);
+  }
+};
+
 Blockly.JavaScript['say_hello'] = function(block) {
   return "console.log('Hello!');\n";
 };
